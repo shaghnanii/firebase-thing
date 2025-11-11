0e9711ac8f35ad42189018d9101fb1622598c160
Shakil Ahmad Khan <shakilsak05@gmail.com>
2025-11-11 22:54:44 +0500

    updated code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  ssr: false,
   devtools: { enabled: true },
   plugins: ['~/plugins/firebase.client.ts'],
-  modules: ['@nuxt/image', '@nuxt/scripts', '@nuxt/ui']
+  modules: ['@nuxt/image', '@nuxt/scripts', '@nuxt/ui'],
+    nitro: {
+    preset: 'static'
+  },
+   app: {
+    baseURL: '/firebase-thing/' // ← IMPORTANT
+  }
 })
